1dad056d771e7d5e515471da4eec0ee8636572a0
wnsgur4322 <32208343+wnsgur4322@users.noreply.github.com>
2019-06-08 13:20:12 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var exphbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
 
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res, next){
