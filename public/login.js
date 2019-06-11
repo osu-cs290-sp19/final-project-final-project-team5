a125c9ec6e9941f21cc49ed8fcf948afbf5597f2
wnsgur4322 <32208343+wnsgur4322@users.noreply.github.com>
2019-06-10 18:31:50 -0700

    Update login.js

--- public/login.js
+++ public/login.js
@@ -1,86 +1,140 @@
 //function for login identification
+var password = ["derek", "tyler", "issac", "eugene"];
+var username = ["Derek", "Tyler", "Isaac", "Eugene"];
+
 function redirect(){
   window.location ="Index.html";
   return false;
 }
 
 function check(){
-  if(document.getElementById('userid').value == "derek" && document.getElementById('pswrd').value == "cs290"){
-    //redirect();
-    window.open ('Index.html');
+  var redirect_true = 0;
+  for(var i = 0; i < username.length; i++){
+    username[i] = username[i].toLowerCase();
+
+    if(document.getElementById('userid').value == username[i] && document.getElementById('pswrd').value == password[i]){
+      //redirect();
+      redirect_true = 1;
+    }
   }
-  else{
-    alert("Check your password or username!")
+  if(redirect_true == 1){
+    window.open("Index.html");
+  }
+  if(redirect_true == 0){
+    alert("Check your password or username!");
   }
 }
+
 
 var login_button = document.querySelector(".Login_button");
 login_button.addEventListener('click', check);
 
+//change password modal open part (remove hidden)
+var modal = document.querySelector(".modal");
+var modal_button = document.querySelector("#create-changepwd-button");
 
-/*$(document).ready(function(){
-   $("#regBtn").click(function(){
-       $.ajax({
-         type : 'GET',
-         url : '/register',
-         success: function(data){
-           $("#regDiv").html(data);
-         }
-       });
-   });
-   $("#loginBtn").click(function(){
-       $.ajax({
-         type : 'GET',
-         url : '/login',
-         success: function(data){
-           $("#loginDiv").html(data);
-         }
-       });
-   });
-   //=====Login Form Request=============================================
-   $("#loginForm").click(function(){
-     var uname  = $("#uname").val();
-     var upass = $("#upass").val();
-     var loginData ={'name': uname,'pass':upass};
-     $.ajax({
-         type : 'POST',
-         url : '/demo',
-         data : loginData,
-         success: function(data){
-         $("#mainDiv").html(data);
-         }
-       });
-   });
-//=====Register Form=============================================
-   $("#regForm").click(function(){
-     var uname  = $("#uname").val();
-     var upass = $("#upass").val();
-     var regData ={'name': uname,'pass':upass};
-       $.ajax({
-         type : 'POST',
-         url : '/regiterToDb',
-         data : regData,
-         success: function(data){
-         $("#mainDiv").html(data);
-         }
-       });
-   });
-//Save profile Data================================================
-$('#saveBtn').click(function(){
-  var email = $("#email").val();
-  var phone = $("#phone").val();
-  var education = $("#education").val();
-  var aoi = $("#aoi").val();
-  var name = $("#name").val();
-  var pass = $("#pass").val();
-  var profileData = {'email':email,'phone':phone,'education' : education,'aoi':aoi,'name' : name,'pass' : pass};
-  $.ajax({
-    type : 'POST',
-    url : '/completeprofile',
-    data : profileData,
-    success : function(data){
-       $("#mainDiv").html(data);
+function remove_hidden(){
+  document.getElementById("create-changepwd-modal").classList.remove('hidden');
+  document.getElementById("modal-backdrop").classList.remove('hidden');
+}
+
+modal_button.addEventListener('click', remove_hidden);
+
+//change password modal close part (rollback hidden option)
+var cancel_button = document.querySelector(".modal-cancel-button");
+
+function rollback(){
+  document.getElementById("changepwd-username-attribution-input").value = "";
+  document.getElementById("changepwd-attribution-input").value = "";
+  document.getElementById("create-changepwd-modal").classList.add('hidden');
+  document.getElementById("modal-backdrop").classList.add('hidden');
+}
+
+cancel_button.addEventListener('click', rollback);
+
+//create account modal open part (remove hidden)
+var modal_2 = document.querySelector(".modal");
+var modal_button_2 = document.querySelector("#create-account-button");
+
+function remove_hidden_2(){
+  document.getElementById("create-account-modal").classList.remove('hidden');
+  document.getElementById("modal-backdrop-2").classList.remove('hidden');
+}
+
+modal_button_2.addEventListener('click', remove_hidden_2);
+
+//create account password modal close part (rollback hidden option)
+var cancel_button_2 = document.querySelector(".modal-cancel-button-2");
+
+function rollback_2(){
+  document.getElementById("account-username-attribution-input").value = "";
+  document.getElementById("account-password-attribution-input").value = "";
+  document.getElementById("create-account-modal").classList.add('hidden');
+  document.getElementById("modal-backdrop-2").classList.add('hidden');
+}
+
+cancel_button_2.addEventListener('click', rollback_2);
+
+// Change password part
+function password_change(){
+  var identify = 0;
+  var user_key = -1;
+
+  for(var j = 0; j < username.length; j++){
+    username[j] = username[j].toLowerCase();
+    if(document.getElementById("changepwd-username-attribution-input").value == username[j] ){
+      identify = 1;
+      user_key = j;
     }
-  });
-});
-});*/
+  }
+
+  if(document.getElementById("changepwd-attribution-input").value.length < 5){
+    identify = 0;
+  }
+
+  if(identify == 1){
+    password[user_key] = document.getElementById("changepwd-attribution-input").value;
+    alert("Your password changed successfully!");
+    document.getElementById("create-changepwd-modal").classList.add('hidden');
+    document.getElementById("modal-backdrop").classList.add('hidden');
+  }
+
+  if(identify == 0){
+    alert("Check your username or set new password at least 5 characters!");
+    document.getElementById("changepwd-username-attribution-input").value = "";
+    document.getElementById("changepwd-attribution-input").value = "";
+  }
+}
+
+var changepwd_button = document.querySelector(".modal-changepwd-button");
+changepwd_button.addEventListener('click', password_change);
+
+// create an account part
+function create_account(){
+  var creating_bool = 0;
+
+  if(document.getElementById("account-username-attribution-input").value != "" && document.getElementById("account-password-attribution-input").value != "" ){
+    creating_bool = 1;
+  }
+  if(document.getElementById("account-password-attribution-input").value.length < 5){
+    creating_bool = 0;
+  }
+
+  if(creating_bool == 1){
+    username.push(document.getElementById("account-username-attribution-input").value);
+    password.push(document.getElementById("account-password-attribution-input").value);
+    document.getElementById("create-account-modal").classList.add('hidden');
+    document.getElementById("modal-backdrop-2").classList.add('hidden');
+    console.log(username, password);
+  }
+
+  if(creating_bool == 0){
+    alert("check your password length or don't left any blank for creating your account !");
+    document.getElementById("account-username-attribution-input").value = "";
+    document.getElementById("account-password-attribution-input").value = "";
+
+  }
+}
+
+var create_account_button = document.querySelector(".modal-account-button");
+create_account_button.addEventListener('click', create_account);
